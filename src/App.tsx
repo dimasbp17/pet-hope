88d58c21f2b4df922a49676e45485ff7a188b990
dimasbp17 <dimas.prasetyo@students.amikom.ac.id>
2025-01-06 21:15:57 +0700

    first setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@mui/material';
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero cumque
+        magnam molestias veritatis assumenda eum possimus, hic, et dolores
+        quidem dicta ad nihil recusandae error quia temporibus quisquam iste
+        deleniti.
+        <Button
+          variant="contained"
+          className="!bg-red-500 !capitalize"
+        >
+          Juh
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default Home;
