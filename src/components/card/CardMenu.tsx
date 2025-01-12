42ba50b65c97a1aeebf5245c6332cc7f92caa730
dimasbp17 <dimas.prasetyo@students.amikom.ac.id>
2025-01-12 21:19:15 +0700

    update:homepage

--- src/components/card/CardMenu.tsx
+++ src/components/card/CardMenu.tsx
@@ -1,9 +1,16 @@
-const CardMenu: React.FC = () => {
+interface CardMenuProps {
+  icon: React.ReactNode;
+  description: string;
+}
+
+const CardMenu: React.FC<CardMenuProps> = ({ icon, description }) => {
   return (
     <>
-      <div className="w-full p-5 text-white bg-orange-600 rounded-lg shadow-lg">
-        <h1>ddd</h1>
-        <h1>ddd</h1>
+      <div className="flex flex-col items-center justify-center w-full p-5 text-white bg-orange-600 rounded-lg shadow-lg">
+        <div className="flex items-center justify-center text-orange-500 bg-white rounded-full size-20">
+          {icon}
+        </div>
+        <h1 className="mt-5">{description}</h1>
       </div>
     </>
   );
